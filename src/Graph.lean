2a3328388fe86168b661382a4d0f30e50fcd0d66
InvisOn <InvisOn@users.noreply.github.com>
2026-05-23 11:48:26 +1200

    improve naming and add dir

--- src/Graph.lean
+++ src/Graph.lean
@@ -14,70 +14,69 @@ structure DiGraph where
 namespace DiGraph
 
 
-def addEdge (graph : DiGraph) (target : String) (dep : Option String) : DiGraph :=
-  match dep with
-  | some dep => { 
-      adjacency := graph.adjacency.getD target {} |> (dep :: ·) |> graph.adjacency.insert target
-    }
-  | none => {
-      adjacency := graph.adjacency.getD target {} |> graph.adjacency.insert target 
-    }
-
-
-def addEdges (graph : DiGraph) (target : String) (deps : Option (List String)) : DiGraph :=
-  match deps with
-    | some deps => deps.foldl (fun graph' dep => addEdge graph' target (some dep)) graph
-    | none => graph.addEdge target none
-
-
-def reverseEdges (graph : DiGraph) : DiGraph :=
-  reverse graph.adjacency.toList {}
-where
-  reverse (adjacency : List (String × List String)) (acc : DiGraph) : DiGraph :=
-    match adjacency with
-      | (target, []) :: rest => acc.addEdge target none |> reverse rest
-      | (target, deps) :: rest => deps.foldl (fun graph' dep => addEdge graph' dep (some target)) acc |> reverse rest
-      | [] => acc
-
-
-def toString (graph : DiGraph) : String :=
-    let entries := graph.adjacency.toList.mapTR fun (target, deps) =>
-    s!"{target} -> [{", ".intercalate deps}]"
-    "DiGraph {\n" ++ 
-    "\n".intercalate entries ++ 
-    "\n}"
-
-
-def toDotNodes (graph : DiGraph) : String :=
-  createNodes graph.adjacency.toList []
-where
-  createNodes (adjacency : List (String × List String)) (acc : List String) : String :=
-    match adjacency with
-      | [] => "\n".intercalate acc
-      | (target, []) :: tail => createNode target none :: acc |> createNodes tail
-      | (target, deps) :: tail => createNodes tail (deps.eraseDups.mapTR (fun dep => createNode target (some dep)) ++ acc)
-
-  
-  @[always_inline]
-  createNode (target : String) (dep : Option String) : String :=
+  def addTarget (graph : DiGraph) (target : String) (dep : Option String) : DiGraph :=
     match dep with
-     | none => s!"  \"{target}\""
-     | some dep => s!"  \"{target}\" -> \"{dep}\""
+    | some dep => { 
+        adjacency := graph.adjacency.getD target {} |> (dep :: ·) |> graph.adjacency.insert target
+      }
+    | none => {
+        adjacency := graph.adjacency.getD target {} |> graph.adjacency.insert target 
+      }
 
 
-/-- Function will always termiante because a Makefile is always a DAG -/
-partial def dependenciesOnly (node : String) (graph : DiGraph) : DiGraph :=
-  findDependencies [node] {}
-where
-  adjacency := graph.adjacency
+  def addTargets (graph : DiGraph) (target : String) (deps : Option (List String)) : DiGraph :=
+    match deps with
+      | some deps => deps.foldl (fun graph' dep => addTarget graph' target (some dep)) graph
+      | none => graph.addTarget target none
 
-  findDependencies (targets : List String) (depsGraph : DiGraph) : DiGraph :=
-    match targets with
-      | [] => depsGraph
-      | node :: deps => findDependencies (deps ++ targets) (depsGraph.addEdges node adjacency[node]?)
 
-partial def targetsOnly (graph : DiGraph) (node : String) : DiGraph :=
-  graph.reverseEdges |>.dependenciesOnly node
+  def reverseEdges (graph : DiGraph) : DiGraph :=
+    reverse graph.adjacency.toList {}
+  where
+    reverse (adjacency : List (String × List String)) (acc : DiGraph) : DiGraph :=
+      match adjacency with
+        | (target, []) :: rest => acc.addTarget target none |> reverse rest
+        | (target, deps) :: rest => deps.foldl (fun graph' dep => addTarget graph' dep (some target)) acc |> reverse rest
+        | [] => acc
+
+
+  def toString (graph : DiGraph) : String :=
+      let entries := graph.adjacency.toList.mapTR fun (target, deps) =>
+      s!"{target} -> [{", ".intercalate deps}]"
+      "DiGraph {\n" ++ 
+      "\n".intercalate entries ++ 
+      "\n}"
+
+
+  def toDotNodes (graph : DiGraph) : String :=
+    createNodes graph.adjacency.toList []
+  where
+    createNodes (adjacency : List (String × List String)) (acc : List String) : String :=
+      match adjacency with
+        | [] => "\n".intercalate acc
+        | (target, []) :: tail => createNode target none :: acc |> createNodes tail
+        | (target, deps) :: tail => createNodes tail (deps.eraseDups.mapTR (fun dep => createNode target (some dep)) ++ acc)
+
+    @[always_inline]
+    createNode (target : String) (dep : Option String) : String :=
+      match dep with
+       | none => s!"  \"{target}\""
+       | some dep => s!"  \"{target}\" -> \"{dep}\""
+
+
+  /-- Will always termiante because a Makefile is always a DAG -/
+  partial def getAllAncesters (node : String) (graph : DiGraph) : DiGraph :=
+    findAllAncestors [node] {}
+  where
+    adjacency := graph.adjacency
+
+    findAllAncestors (targets : List String) (depsGraph : DiGraph) : DiGraph :=
+      match targets with
+        | [] => depsGraph
+        | target :: rest => findAllAncestors (rest ++ targets) (depsGraph.addTargets target adjacency[target]?)
+
+  partial def getAllDescendents (graph : DiGraph) (target : String) : DiGraph :=
+    graph.reverseEdges |>.getAllAncesters target
 
 
 end DiGraph
@@ -86,10 +85,10 @@ end DiGraph
 instance : ToString DiGraph := ⟨DiGraph.toString⟩
 
 
-def addDotHeader (dotNodes : String) (rankdir : String) : String := s!"digraph G \{
+def addDotHeader (dotNodes : String) (rankdir : String) (dir : String): String := s!"digraph G \{
   graph [rankdir={rankdir}]
   node [shape=box, style=solid, margin=\"0.3,0.1\"]
-  edge [color=\"#00000088\", dir=back, penwidth=1.2, minlen=1]
+  edge [color=\"#00000088\", dir={dir}, penwidth=1.2, minlen=1]
 
 {dotNodes}
 }"
@@ -107,9 +106,9 @@ where
         if head.startsWith "#" then
           parseRules tail acc
         else
-          match head.splitOn "\n" |>.take 1 |>.getD 0 "" |>.splitOn ":" with
+          match head.splitOn "\n" |>.getD 0 "" |>.splitOn ":" with
             | ".PHONY" :: _ => parseRules tail acc
-            | target :: deps :: [] => parseDeps deps |> acc.addEdges target |> parseRules tail
+            | target :: deps :: [] => parseDeps deps |> acc.addTargets target |> parseRules tail
             | _ => parseRules tail acc
 
   @[always_inline]
@@ -119,8 +118,8 @@ where
     | deps => some (deps.splitOn.dropWhile (fun (s : String) => s.length == 0))
 
 
--- TODO: implement only show ancesters and/or descendents of name pattern
--- TODO: implement coloring by name pattern
+-- TODO: implement only show ancesters and/or descendents of name
+-- TODO: implement coloring by name
 -- TODO: add unit tests
 -- TODO: prove some stuff about the code maybe
 
--- src/Main.lean
+++ src/Main.lean
@@ -5,11 +5,13 @@ import Graph
 def main (_args : List String) : IO UInt32 := do
   let makeP <- getStdin
   let rankdir := "RL"
+  let dir := "forward"
 
   match parseMakeP makeP |>.toDotNodes with
     | "" => 
       IO.println "No viable graph"
       return 1
     | dotNodes =>
-      addDotHeader dotNodes rankdir |> IO.println
+      addDotHeader dotNodes rankdir dir |> IO.println
       return 0
+
